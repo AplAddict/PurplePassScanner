6ed23446ff7f5f1bde8ba20ab6d1525ddfe86b9b
Isaac (AplAddict) <izip14@gmail.com>
2023-05-18 17:41:50 -0500

    Inventory

--- docs/app.js
+++ docs/app.js
@@ -10,7 +10,22 @@ var app = new Vue({
     var self = this;
     self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
+<<<<<<< Updated upstream
       self.scans.unshift({ date: +(Date.now()), content: content });
+=======
+      if ((content.split("-")[0].length = 7) && (content.split("-")[1].length = 2) && (content.split("-")[2].length = 1)) {
+        self.scans.unshift({ date: +(Date.now()), content: content });
+        fetch("https://hook.us1.make.com/tfme118y55za9wgcebxwau5u8jy3664p", {
+          method: "POST",
+          body: JSON.stringify({
+            Note: content + ": " + prompt('Enter +/- and then the number you would like to add/subtract from inventory.', '') + ": "
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+        });
+      }
+>>>>>>> Stashed changes
     });
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
